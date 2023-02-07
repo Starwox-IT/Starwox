20d2a815e71596355d4a89944e9122c0c7408456
promise <uwagboepromise5@gmail.com>
2023-02-07 14:37:00 +0100

    Added little animation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Web from 'components/Web/Web';
 import Data from 'components/Data/Data';
 import IT from 'components/IT/IT';
 import './App.css';
-// import {gsap, TimelineLite, Power3 } from 'gsap';
-// import { useEffect } from "react";
+
 
 function App() {
 
-  // let tl = new TimeLineLite({ delay: 0.3})
-  // useEffect(() => {
-  //   tl.from('.hero-text', {y:15, opacity: 0, ease:Power3.easeOut}, 'start')
-  // }, [])
+  
     return <div className='App'>
       <Routes>
         <Route path="/" element={<Home/>}/>
--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import {gsap, Power4 } from 'gsap';
+import React, { useEffect } from "react";
 import './Hero.css'
 import illu1 from 'assets/illu 1.png';
 import rightarrow from 'assets/right-arrow.png';
@@ -6,6 +7,13 @@ import { doingsData } from 'Data/Doings';
 import Doings from '../Doings/Doings';
 
 const Hero = () => {
+    useEffect(()=> {
+        let tl = gsap.timeline({defaults: {ease: Power4.easeInOut, duration:2}})
+        tl.to('.hero-img', {opacity:1, 'transform': 'scale(1)'})
+            .to('.main-hero-text', {'clip-path': 'polygon(0% 100%, 99% 100%, 100% 0%, 0% 0%)', opacity:1, y:0, duration:2.2}, '-=2')
+    }, [])
+    
+
     const doings = doingsData.map(doing => {
         return (
             <Doings
@@ -18,17 +26,21 @@ const Hero = () => {
         )
     })
     
+    // let tl = new TimelineLite({ delay: 0.3})
+    // useEffect(() => {
+    //     tl.from('.hero-text', {y:15, opacity: 0, ease:Power3.easeOut, delay:0.2}, 'start')
+    // }, [])
 
   return (
     <div className="hero">
         <div className="hero-wrapper">
             <div className="hero-top">
-                <div className="hero-text">
+                <div className="hero-text main-hero-text">
                     <h1>Your <span className="effi">efficient</span> team <br/> beyond walls</h1>
                     <p>We make business run smoothly by handling your digital processes smoothly with team work and strategic planning. </p>
                 </div>
 
-                <img src={illu1} alt="" />
+                <img src={illu1} alt="" className='hero-img'/>
             </div>
 
             <div className="hero-bottom" id='services'>
