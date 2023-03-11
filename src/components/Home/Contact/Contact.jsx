79e0100f9ea77b18279c7401b2d74c23ce2da34c
promise <uwagboepromise5@gmail.com>
2023-03-11 12:46:34 +0100

    Latest update

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -20,27 +20,27 @@ const Contact = () => {
     <div className="contact">
         <div className="contact-wrapper">
             <div className="contact-heading">
-                <h1><span className="lets">Let’s </span>Chat</h1>
+                <h1 className="form-heading">Let’s Chat</h1>
                 <p>Let’s create something great together!</p>
             </div>
 
             <div className="contact-main">
                 <form action="" id='form'>
                     <div className="name contact-same">
-                        <label htmlFor="name">Your Name</label>
-                        <input type="text" id='name' name='name'/>
+                        {/* <label htmlFor="name">Your Name</label> */}
+                        <input type="text" id='name' name='name'placeholder="Your Name"/>
                     </div>
                     <div className="name contact-same">
-                        <label htmlFor="phone">Phone Number</label>
-                        <input type="text" id='phone' name='phone'/>
+                        {/* <label htmlFor="phone">Phone Number</label> */}
+                        <input type="text" id='phone' name='phone' placeholder="Phone Number"/>
                     </div>
                     <div className="name contact-same">
-                        <label htmlFor="address">Email Address</label>
-                        <input type="text" id='address' name='address'/>
+                        {/* <label htmlFor="address">Email Address</label> */}
+                        <input type="text" id='address' name='address' placeholder="Email Address"/>
                     </div>
                     <div className="name contact-same">
-                        <label htmlFor="about">Tell us about your project</label>
-                        <input type="text" id='about' name='about'/>
+                        {/* <label htmlFor="about">Tell us about your project</label> */}
+                        <input type="text" id='about' name='about' placeholder="Type your message..."/>
                     </div>
 
                     <div className="submit">
--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -1,86 +1,84 @@
 import React from 'react'
 import './Footer.css'
-import Logo from 'assets/logo.png';
+import { NavLink } from 'react-router-dom'
+import footerLogo from 'assets/footer-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { HashLink } from "react-router-hash-link"
 
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-wrapper">
-        <div className="logo">
-          <img src={Logo} alt="" />
+        <div className="social">
+          <img src={footerLogo} alt="" />
+          <p className="email">starwox1@gmail.com</p>
+          <div className="icons">
+            <a href="https://web.facebook.com" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faFacebook}/>
+            </a>
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faInstagram}/>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faTwitter}/>
+            </a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin}/>
+            </a>
+          </div>
         </div>
-        
-        <div className="reach">
-          <h3>GET IN TOUCH</h3>
-          <div className="reach-general">
-            <div className="reach-left">
-              <div className="reach-left-top">
-                <p className="email">starwox1@gmail.com</p>
-                <p className="phone-num1">+447840361050</p>
-                <p className="phone-num2">+2348135651507</p>
-              </div>
 
-              <div className="reach-left-bottom">
-                <h3>KNOW MORE</h3>
+        <div className="numbers">
+          <h4>Phone Number</h4>
+          <p className="number1">+447840361050</p>
+          <p className="number2">+2348135651507</p>
+        </div>
 
-                <div className="footer-nav">
-                  <ul className="footer-nav-left">
-                    <li ><a href="#top" className='homes'>Portfolio</a></li>
+        <div className="office">
+          <div className="head-office">
+            <h4>Head Office</h4>
+            <p>72 Glenkirk Drive, G15 6AH, Glasgow, United Kingdom</p>
+          </div>
+          <div className="branch-office">
+            <h4>Branch Office</h4>
+            <p>244 New Lagos Road, Benin City, Edo State 300001</p>
+          </div>
+        </div>
 
-                    <li><a className='abouts' href="#abouts">About</a></li>
-    
-                    <li><a className='resumes' href="#resumes">Contact</a></li>
-                  </ul>
+        <div className="footer-links">
+          <div className="main-footer-links">
+            <h4>Company</h4>
+            <ul className="navbar-nav">
+              <NavLink to='/'>Home</NavLink>
 
-                  <ul className="footer-nav-right">
-                    <li ><a href="#top" className='homes'>Services</a></li>
-                    <li><a className='abouts' href="#abouts">Blog</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="location">
-              <p>72 Glenkirk Drive, G15 6AH, Glasgow, United Kingdom</p>
-              <p>244 New Lagos Road, Benin City, Edo State 300001</p>
+              <NavLink to='/h'>Portfolio</NavLink>
+              
+              <NavLink to='/about'>About</NavLink>
+
+              <HashLink to="/#services">Services</HashLink>
+            </ul>
+          </div>
+
+          <div className="resource">
+            <h4>Resources</h4>
+            <div className="resource-main">
+              <p>Privacy policy</p>
+              <p className="dot1">.</p>
+              <p>Cookie policy</p>
+              <p className="dot2">.</p>
+              <p>Terms and conditions</p>
             </div>
           </div>
-          
-        </div>
-
-        
-
-        <div className="icons">
-          <a href="https://web.facebook.com/ikponmwoba.eloghosa" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faFacebook}/>
-          </a>
-          <a href="https://www.instagram.com/el0tech/" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faInstagram}/>
-          </a>
-          <a href="https://twitter.com/eL0TECH" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faTwitter}/>
-          </a>
-          <a href="https://www.linkedin.com/in/eloghosa-ikponmwoba/" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faLinkedin}/>
-          </a>
         </div>
       </div>
 
       <div className="copyright">
-        <div className="copy-privacy">
-          <p>C 2022 Starwox All Rights Reserved </p>
-          <p>Privacy policy <span className="dot">.</span></p>
-        </div>
-        
-        <div className="cookie-terms">
-          <p> Cookie policy <span className="dot">.</span></p>
-          <p>Terms and conditions</p>
-        </div>
+        <p>&#169; 2022 Starwox All Rights Reserved </p>
       </div>
     </div>
   )
